87f41e9fce4ac161bdf6f6e76b7bc09ceb382dfc
billmyers223 <billymyers223@gmail.com>
2021-09-06 12:38:04 -0500

    AddOne works

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, IMPOSE_MEMORY, CLEAR_MEMORY, ADD_MEMORY } from './../actions';
 
 export const initialState = {
     total: 100,
@@ -37,6 +37,30 @@ const reducer = (state, action) => {
                 operation: action.payload
             });
             
+        case (CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            });
+
+        case (IMPOSE_MEMORY):
+            return ({
+                ...state,
+                total: calculateResult(state.total, state.memory, state.operation)
+            });
+
+        case (CLEAR_MEMORY):
+            return ({
+                ...state,
+                memory: 0
+            })
+
+        case (ADD_MEMORY):
+            return ({
+                ...state,
+                memory: state.total
+            })
+
         default:
             return state;
     }
